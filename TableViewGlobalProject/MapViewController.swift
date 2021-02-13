cf3a1ade98cadfd58273f61c70da5e5b63d3c6de
EreskinVA <ereskinva@icloud.com>
2021-02-14 01:08:51 +0300

    подготовка для работы с MapKitView

---
+++ TableViewGlobalProject/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  TableViewGlobalProject
+//
+//  Created by 17795838 on 14.02.2021.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+	@IBAction func closeMap() {
+		dismiss(animated: true)
+	}
+}
